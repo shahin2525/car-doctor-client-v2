2bf4e9f6dac8c1d83e7f4d6263bdc53f1026c24d
shahin2525 <shahin451999@gmail.com>
2024-11-16 08:45:37 -0800

    add redirectd url

--- src/Routes/PrivateRoute.jsx
+++ src/Routes/PrivateRoute.jsx
@@ -1,19 +1,21 @@
-import { useContext } from 'react';
-import { AuthContext } from '../providers/AuthProvider';
-import { Navigate } from 'react-router';
+import { useContext } from "react";
+import { AuthContext } from "../providers/AuthProvider";
+import { Navigate, useLocation } from "react-router";
 
-const PrivateRoute = ({children}) => {
-    const {user, loading} = useContext(AuthContext);
+const PrivateRoute = ({ children }) => {
+  const { user, loading } = useContext(AuthContext);
+  const location = useLocation();
+  console.log(location);
 
-    if(loading) {
-        return <progress className="progress w-56"></progress>
-    }
+  if (loading) {
+    return <progress className="progress w-56"></progress>;
+  }
 
-    if(user?.email){
-        return children;
-    }
+  if (user?.email) {
+    return children;
+  }
 
-    return <Navigate to="/login" replace></Navigate>;
+  return <Navigate state={location.pathname} to="/login" replace></Navigate>;
 };
 
 export default PrivateRoute;
