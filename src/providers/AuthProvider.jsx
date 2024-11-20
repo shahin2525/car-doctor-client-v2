e1d0447e3bf75855e288835307865a20ea698bd6
shahin2525 <shahin451999@gmail.com>
2024-11-20 10:42:26 -0800

    add cookie

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -33,12 +33,22 @@ const AuthProvider = ({ children }) => {
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+      const userEmail = currentUser?.email || user?.email;
+      const loggedUser = { email: userEmail };
       setUser(currentUser);
       console.log("current user", currentUser);
-      const loggedUser = { email: currentUser?.email };
+
       if (currentUser) {
         axios
           .post("http://localhost:5000/jwt", loggedUser, {
+            withCredentials: true,
+          })
+          .then((res) => {
+            console.log(res.data);
+          });
+      } else {
+        axios
+          .post("http://localhost:5000/logout", loggedUser, {
             withCredentials: true,
           })
           .then((res) => {
